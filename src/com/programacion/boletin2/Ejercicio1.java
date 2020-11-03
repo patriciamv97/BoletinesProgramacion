c44887b7c3931198c4011dd840de7d31fa11ed89
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-03 17:51:05 +0100

    Primer ejercicio Boletin 2

---
+++ src/com/programacion/boletin2/Ejercicio1.java
@@ -0,0 +1,9 @@
+package com.programacion.boletin2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        short base=4, altura=3, area;
+        area = (short) (base*altura/2);
+        System.out.println("El area del triangulo es igual a "+area);
+    }
+}
