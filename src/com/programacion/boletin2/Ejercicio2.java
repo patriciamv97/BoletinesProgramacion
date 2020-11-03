b48a4ed08a1ff7ae1e75b96fa10d76658d621968
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-03 17:51:50 +0100

    Segundo ejercicio Boletin 2

---
+++ src/com/programacion/boletin2/Ejercicio2.java
@@ -0,0 +1,9 @@
+package com.programacion.boletin2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        short lado=3, area;
+        area= (short) (lado*lado);
+        System.out.println("El area del rectangulo es "+ area);
+    }
+}
