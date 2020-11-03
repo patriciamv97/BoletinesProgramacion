06e0f301b82576e538aaf21e8b41cc78d2214032
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-03 19:34:28 +0100

    Apartado 2 ejercicio 6

---
+++ src/com/programacion/boletin06/Ejercicio2.java
@@ -0,0 +1,8 @@
+package com.programacion.boletin06;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Satelite obx= new Satelite (3,4,60000);
+        obx.verPosicion();
+    }
+}
---
+++ src/com/programacion/boletin06/Satelite.java
@@ -0,0 +1,25 @@
+package com.programacion.boletin06;
+
+public class Satelite {
+    //Atributos
+    private double meridiano;
+    private double paralelo;
+    private double distanciaTierra;
+
+    //Constructores
+    public Satelite(){
+    }
+
+    public Satelite (double m ,double p, double d){
+        meridiano=m;
+        paralelo=p;
+        distanciaTierra=d;
+    }
+
+    public void verPosicion(){
+        System.out.println("O satelite atopase no paralelo " +( paralelo) + " o meridiano no " + (meridiano)+ " a unha distancia da Terra "+ (distanciaTierra)+" km");
+    }
+
+
+
+}
