0ecef618142b9851164425da465493ad7c8a9bfe
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-06 09:42:48 +0100

    Comentarios para java doc

--- src/com/programacion/boletin06/ejercicio3/Circulo.java
+++ src/com/programacion/boletin06/ejercicio3/Circulo.java
@@ -1,40 +1,67 @@
 package com.programacion.boletin06.ejercicio3;
 
+/**
+ * Unha clase para calcular o area dun circulo e a lonxitude da circunferencia
+ * @author Patricia Maceiras
+ * @version 0.1.1
+ */
+
 public class Circulo {
     //Atributos
 
     private double radio;
     public final double PI=3.14;
 
-    // Metdos Constructores
-    // Metodo constructor sen paramaetros
+    /**
+     * Metodo constructor que xenera un circulo cos seus parametros a cero o null
+     */
+
     public Circulo (){
 
     }
-    //Metdodo constructor con parametros
+
+    /**
+     * Crea un círculo a partir de su origen su radio
+     * @param r
+     */
+
     public Circulo (double r){
         radio=r;
 
     }
 
-    //Metodos Acceso
+    /**
+     * Metodo que se utliza para dar un valor ao radio
+     * @param r
+     */
 
     public void setRadio(double r){
         radio=r;
 
     }
+
+    /**
+     * Metdodo que se utiliza para obter o radio
+     * @return devolve o radio
+     */
     public double getRadio (){
 
         return radio;
     }
 
-
-    //Metodos calcular
+    /**
+     * calcula o area do circulo co radio
+     * @return retorna o area do circulo
+     */
     public double calcularArea(){
         double area;
         area=Math.pow(radio,2)*PI;
         return area;
     }
+    /**
+     * calcula a lonxitude da circunferencia
+     * @return retorna a lonxitude da circunferencia
+     */
     public double calcularLonxitude (){
         double lonxitude;
         lonxitude = 2*radio*Math.PI;
