15cde700a04aef0bf63d8c7789cefe41b7fff037
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-12 13:40:41 +0100

    Seguimento pesos

---
+++ src/com/programacion/boletin07/Apartado4.java
@@ -0,0 +1,16 @@
+package com.programacion.boletin07;
+
+import javax.swing.*;
+
+public class Apartado4 {
+    public static void main(String[] args) {
+        String n1 = JOptionPane.showInputDialog("Introduce o nome da primeira persoa ");
+        int p1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce o peso da primeria persoa"));
+        String n2 = JOptionPane.showInputDialog("Introduce o nome da segunda persoa");
+        int p2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce o peso da segunda persoa"));
+        Peso obx = new Peso();
+        obx.seguimentoPeso(p1,p2,n1,n2);
+
+
+    }
+}
