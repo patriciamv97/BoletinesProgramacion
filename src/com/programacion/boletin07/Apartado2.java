2edc712da31cc3d2b53f4eac7499cf9f4776e2a4
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-12 13:08:32 +0100

    Suma resta numeros short

---
+++ src/com/programacion/boletin07/Apartado2.java
@@ -0,0 +1,14 @@
+package com.programacion.boletin07;
+
+import javax.swing.*;
+
+public class Apartado2 {
+    public static void main(String[] args) {
+        NumerosShort obx = new NumerosShort();
+        short n1 = Short.parseShort(JOptionPane.showInputDialog("Introduce o primeiro número"));
+        short n2 =  Short.parseShort(JOptionPane.showInputDialog("Introduce o segundo número"));
+        obx.comparaNumeros(n1,n2);
+
+
+    }
+}
---
+++ src/com/programacion/boletin07/NumerosShort.java
@@ -0,0 +1,12 @@
+package com.programacion.boletin07;
+
+import javax.swing.*;
+
+public class NumerosShort {
+    public void comparaNumeros (short n1 , short n2){
+        if (n1>n2)
+            JOptionPane.showMessageDialog(null,"A resta é "+ (n1- n2) + "\nA suma é "+(n1+n2));
+        else
+            JOptionPane.showMessageDialog(null,"A suma é "+(n1+n2));
+    }
+}
