6851a8d51ea8443279a8eeb8043eccf7891cebbd
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-12 13:40:44 +0100

    Seguimento pesos

---
+++ src/com/programacion/boletin07/Peso.java
@@ -0,0 +1,12 @@
+package com.programacion.boletin07;
+
+import javax.swing.*;
+
+public class Peso {
+    public void seguimentoPeso (int p1,int p2,String n1,String n2){
+        if (p1>p2)
+            JOptionPane.showMessageDialog(null,n1 + " pesa máis e o seu peso é "+p1 + ";\na diferenza de pesos é " +(p1-p2) );
+        else
+            JOptionPane.showMessageDialog(null,n2 + " pesa máis e o seu peso é "+p2 + ";\na diferenza de pesos é " +(p2-p1) );
+    }
+}
