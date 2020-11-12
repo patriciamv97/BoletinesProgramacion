08a7b233de515f0ebe99eb0aa616be0a2feb90ac
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-12 14:37:55 +0100

    Solucionado error

---
+++ src/com/programacion/boletin08/ejercicio6/Ejercicio6.java
@@ -0,0 +1,13 @@
+package com.programacion.boletin08.ejercicio6;
+
+import javax.swing.*;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String nomeArtigo = JOptionPane.showInputDialog("Introduce o nome do artigo");
+        float totalVendas = Float.parseFloat(JOptionPane.showInputDialog("Introduce o total de vendas anuais"));
+        VendasAnuais obx = new VendasAnuais();
+        obx.clasificar(nomeArtigo,totalVendas);
+
+    }
+}
---
+++ src/com/programacion/boletin08/ejercicio6/VendasAnuais.java
@@ -0,0 +1,19 @@
+package com.programacion.boletin08.ejercicio6;
+
+import javax.swing.*;
+
+public class VendasAnuais {
+    public void clasificar (String nomeArtigo , float totalVendas){
+        if (totalVendas<=100)
+            JOptionPane.showMessageDialog(null," o consumo de "+nomeArtigo+" é baixo");
+        else
+            if (totalVendas>100 & totalVendas<=500)
+                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é medio");
+            else
+                if (totalVendas>500 & totalVendas<=1000)
+                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é alto");
+                else
+                JOptionPane.showMessageDialog(null,nomeArtigo+" é de primeira necesidade");
+
+    }
+}
