8e7e0c68f9dd073e631f4c8de5dd6a478e81955e
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-12 14:39:19 +0100

    EJercicio 6 boletin 8 terminado

--- src/com/programacion/boletin08/ejercicio6/Ejercicio6.java
+++ src/com/programacion/boletin08/ejercicio6/Ejercicio6.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class Ejercicio6 {
     public static void main(String[] args) {
-        String nomeArtigo = JOptionPane.showInputDialog("Introduce o nome do artigo");
-        float totalVendas = Float.parseFloat(JOptionPane.showInputDialog("Introduce o total de vendas anuais"));
+        String nomeArtigo = JOptionPane.showInputDialog("Introduce o nome do artigo :");
+        float totalVendas = Float.parseFloat(JOptionPane.showInputDialog("Introduce o total de vendas anuais :"));
         VendasAnuais obx = new VendasAnuais();
         obx.clasificar(nomeArtigo,totalVendas);
 
--- src/com/programacion/boletin08/ejercicio6/VendasAnuais.java
+++ src/com/programacion/boletin08/ejercicio6/VendasAnuais.java
@@ -5,15 +5,15 @@ import javax.swing.*;
 public class VendasAnuais {
     public void clasificar (String nomeArtigo , float totalVendas){
         if (totalVendas<=100)
-            JOptionPane.showMessageDialog(null," o consumo de "+nomeArtigo+" é baixo");
+            JOptionPane.showMessageDialog(null," o consumo de "+nomeArtigo+" é baixo.");
         else
             if (totalVendas>100 & totalVendas<=500)
-                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é medio");
+                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é medio.");
             else
                 if (totalVendas>500 & totalVendas<=1000)
-                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é alto");
+                JOptionPane.showMessageDialog(null,"O consumo de "+nomeArtigo+" é alto.");
                 else
-                JOptionPane.showMessageDialog(null,nomeArtigo+" é de primeira necesidade");
+                JOptionPane.showMessageDialog(null,nomeArtigo+" é de primeira necesidade.");
 
     }
 }
