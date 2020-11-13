71f4a1443f9d69db825f922d2cae6e51d49c149a
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-13 20:32:59 +0100

    ejercicio 8 terminado

--- src/com/programacion/boletin08/ejercicio7/Menu.java
+++ src/com/programacion/boletin08/ejercicio7/Menu.java
@@ -1,7 +1,13 @@
 package com.programacion.boletin08.ejercicio7;
-
+import javax.swing.*;
 public class Menu {
     public static void main(String[] args) {
+        Superficies obx = new Superficies();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opción \ncuadrado          triangulo          circulo\n debe introducir 1, 2 o 3"));
+        if (num ==1 || num==2 || num==3)
+            obx.calularAreas(num);
+        else
+            JOptionPane.showMessageDialog(null,"Opción incorrecta");
 
     }
 }
--- src/com/programacion/boletin08/ejercicio7/Superficies.java
+++ src/com/programacion/boletin08/ejercicio7/Superficies.java
@@ -1,5 +1,30 @@
 package com.programacion.boletin08.ejercicio7;
 
+import javax.swing.*;
+
 public class Superficies {
+
+    public void calularAreas(int num ) {
+        switch (num) {
+            case 1:
+                float l = Float.parseFloat(JOptionPane.showInputDialog("Introduce  o lado do cadrado : "));
+                JOptionPane.showMessageDialog(null,"O area do cadrado é : "+ Math.pow(l,2) +" unidades cadradas");
+            break;
+            case 2 :
+                float a = Float.parseFloat(JOptionPane.showInputDialog("Introduce a altura do triangulo "));
+                float b = Float.parseFloat(JOptionPane.showInputDialog("Introduce a base do triangulo "));
+                JOptionPane.showMessageDialog(null,"O área do triangulo é : "+ b*a/2 +" unidades cadradas");
+            break;
+            case 3 :
+                float r = Float.parseFloat(JOptionPane.showInputDialog("Introduce o radio do circulo : "));
+                JOptionPane.showMessageDialog(null,"O área do circulo é : "+Math.pow(r,2)*Math.PI+" unidades cadradas");
+                break;
+            default: JOptionPane.showMessageDialog(null,"Opción incorrecta");
+
+
+        }
+
+
+    }
 
 }
