8f132d3505284479c563dd75f7e536a678da5c39
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-23 10:53:06 +0100

    modificacion

--- src/com/programacion/boletin07/ejercicio4/Peso.java
+++ src/com/programacion/boletin07/ejercicio4/Peso.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class Peso {
     public void seguimentoPeso (int p1,int p2,String n1,String n2){
-        if (p1>p2)
-            JOptionPane.showMessageDialog(null,n1 + " pesa máis e o seu peso é "+p1 + ";\na diferenza de pesos é " +(p1-p2) );
+        if (p1<= 0 || p2<=0)
+            JOptionPane.showMessageDialog(null,"peso erroneo");
         else
+           if (p1>p2)
+            JOptionPane.showMessageDialog(null,n1 + " pesa máis e o seu peso é "+p1 + ";\na diferenza de pesos é " +(p1-p2) );
+          else
             JOptionPane.showMessageDialog(null,n2 + " pesa máis e o seu peso é "+p2 + ";\na diferenza de pesos é " +(p2-p1) );
     }
 }
