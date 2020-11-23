f19ee67c0d1230a6a6764712d4ee4491ba2765c4
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-23 10:14:51 +0100

    Boletin 9 ejercicio 1

---
+++ src/com/programacion/Boletin09/Contador.java
@@ -0,0 +1,23 @@
+package com.programacion.Boletin09;
+
+import com.programacion.BoletinDiaExamen.Docentes;
+
+import javax.swing.*;
+
+public class Contador {
+    public void Contador() {
+        int numero, cero = 0, negativo = 0, positivo = 0;
+        for (int i = 0; i < 10; i++) {
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero"));
+            if (numero >= 0)
+                if (numero == 0)
+                    cero = cero + 1;
+                else
+                    positivo = positivo + 1;
+            else
+                negativo = negativo + 1;
+
+        }
+        JOptionPane.showMessageDialog(null,"O número de positivos é : "+positivo+"\nO número de ceros é : "+cero+" \nO número de negativos é :"+negativo);
+    }
+}
---
+++ src/com/programacion/Boletin09/Principal.java
@@ -0,0 +1,8 @@
+package com.programacion.Boletin09;
+
+public class Principal {
+    public static void main(String[] args) {
+        Contador obx = new Contador();
+        obx.Contador();
+    }
+}
