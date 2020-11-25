79cd5ae28fe83d4b1edcb51c66b628eb793363ca
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-25 17:27:53 +0100

    reamte exercicio 5

---
+++ src/com/programacion/Boletin09/ejercicio5/Principal.java
@@ -0,0 +1,12 @@
+package com.programacion.Boletin09.ejercicio5;
+
+public class Principal {
+    public static void main(String[] args) {
+        Traballadores obx = new Traballadores();
+        obx.soldoTraballadores();
+
+
+
+    }
+
+}
---
+++ src/com/programacion/Boletin09/ejercicio5/Traballadores.java
@@ -0,0 +1,30 @@
+package com.programacion.Boletin09.ejercicio5;
+
+import com.programacion.metodos.Metodos;
+
+import javax.swing.*;
+
+public class Traballadores {
+    public void soldoTraballadores() {
+        float soldo ,porcentaxe ;
+        int traballdoresMileuristas=0,traballadoresPobres=0;
+        do {
+            soldo = Metodos.validacionPositivos("Introduce o soldo :");
+            if (soldo<1000 && soldo>0)
+                traballadoresPobres++;
+            else
+            if  (soldo >= 1000  && soldo<=1750)
+                    traballdoresMileuristas++;
+        }while (soldo != 0) ;
+        porcentaxe= (float) traballadoresPobres/(traballadoresPobres+traballdoresMileuristas)*100;
+        JOptionPane.showMessageDialog(null,"Hai " +traballdoresMileuristas+" traballadres que cobran entre 1000 e 1750 euros");
+        JOptionPane.showMessageDialog(null,"Hai " + traballadoresPobres +" traballadores que cobran menos de 1000" );
+        JOptionPane.showMessageDialog(null,"O "+porcentaxe+"% dos traballadores cobran menos de mil euros.");
+
+
+
+
+
+    }
+}
+
