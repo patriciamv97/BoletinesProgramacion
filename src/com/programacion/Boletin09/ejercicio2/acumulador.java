811fef48bb8ca603a0568fbd8a0a2ca507e85820
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-25 13:19:09 +0100

    exercicio 2 correxido

--- src/com/programacion/Boletin09/ejercicio2/acumulador.java
+++ src/com/programacion/Boletin09/ejercicio2/acumulador.java
@@ -1,20 +1,35 @@
 package com.programacion.Boletin09.ejercicio2;
 
-import javax.swing.*;
 
 public class acumulador {
     public void suma() {
         int suma = 0;
         for (int i = 10; i < 90; i++) {
             suma = suma + i;
-            System.out.println("A suma dos números é :" + suma );
+
         }
+        System.out.println("A suma dos números é :" + suma );
     }
     public  void producto(){
         double p =1 ;
         for (int i =10;i<90;i++){
             p=p*i;
-            System.out.println("O producto dos números é :"+ p);
         }
+        System.out.println("O producto dos números é :"+ p);
+
     }
+    //Se puede simplificar
+    public void resultado (){
+        int s = 0;
+        double p =1;
+
+        for (int i = 10; i < 90; i++) {
+            s = s + i;
+             p = p * i;
+        }
+        System.out.println( p );
+        System.out.println( s );
+
+    }
+
 }
--- src/com/programacion/Boletin09/ejercicio2/ejercicio2.java
+++ src/com/programacion/Boletin09/ejercicio2/ejercicio2.java
@@ -3,8 +3,9 @@ package com.programacion.Boletin09.ejercicio2;
 public class ejercicio2 {
     public static void main(String[] args) {
         acumulador obx = new acumulador();
-        //obx.suma();
-        obx.producto();
+        /*obx.suma();
+        obx.producto();*/
+        obx.resultado();
 
     }
 }
