c9ae5e73a67fdc55529f68796f12bf42b6957d57
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2020-11-26 14:26:50 +0100

    ejercicio1 en proceso

---
+++ src/com/programacion/boletin11/Principal.java
@@ -0,0 +1,8 @@
+package com.programacion.boletin11;
+
+public class Principal {
+    public static void main(String[] args) {
+         Xogo obx = new Xogo();
+         obx.adivinarNumero();
+    }
+}
---
+++ src/com/programacion/boletin11/Xogo.java
@@ -0,0 +1,27 @@
+package com.programacion.boletin11;
+
+import javax.swing.*;
+
+public class Xogo {
+    public void adivinarNumero() {
+        int n, cantidad, n1 , intentos = 0,n2;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Primeiro xogador ,introduce un número entre 1 e 50"));
+        do {
+            n1 = Integer.parseInt(JOptionPane.showInputDialog("Debes introducir un número entre 1 e 50"));
+
+        } while (n1 < 1 || n1 > 50);
+
+            cantidad = Integer.parseInt(JOptionPane.showInputDialog("Xogador 1,introduce o número de intentos que ten o segundo xogador"));
+        do {
+            n2 = Integer.parseInt(JOptionPane.showInputDialog("Xogador 2,¿Que número introduxo o xogador 1?"));
+            for (int i =0;i<=cantidad;i++) {
+                intentos++;
+                if (n2 < n)
+                    JOptionPane.showMessageDialog(null, "O número é maior que o que puxeches ");
+                if (n2 > n)
+                    JOptionPane.showMessageDialog(null, "O número é menor que o que puxeches ");
+            }
+        }while(intentos<cantidad || n1==n );
+    }
+}
+
