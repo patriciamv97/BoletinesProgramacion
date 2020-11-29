e8af60aa1efa30d0e8dfcb7168c20e9efa968599
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-29 12:23:27 +0100

    Ejercicio 1 boletin 11

--- src/com/programacion/boletin11/Principal.java
+++ src/com/programacion/boletin11/Principal.java
@@ -1,8 +1,10 @@
 package com.programacion.boletin11;
 
+
 public class Principal {
     public static void main(String[] args) {
          Xogo obx = new Xogo();
          obx.adivinarNumero();
+
     }
 }
