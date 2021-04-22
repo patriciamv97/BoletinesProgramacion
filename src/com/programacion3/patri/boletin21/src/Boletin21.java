6e71cffd9ca9a4f9ad38268b6724ac8e754cd8b8
patriciamv97 <72247871+patriciamv97@users.noreply.github.com>
2021-04-22 19:02:36 +0200

    Solucion error nombre paquete

---
+++ src/com/programacion3/patri/boletin21/src/Boletin21.java
@@ -0,0 +1,23 @@
+
+import patri.apartadoA;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        apartadoA f =new apartadoA();
+        f.setVisible(true);
+    }
+    
+}
