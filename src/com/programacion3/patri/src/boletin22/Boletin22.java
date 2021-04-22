52cbdf3fb535fa8e56645f835552f81d00981aa5
patriciamv97 <72247871+patriciamv97@users.noreply.github.com>
2021-04-22 18:56:25 +0200

    Boletin 22 subido a github

---
+++ src/com/programacion3/patri/src/boletin22/Boletin22.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin22;
+
+/**
+ *
+ * @author Usuario
+ */
+import patri.Ventana;
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana v =new Ventana();
+        v.setVisible(true);
+        
+        
+    }
+    
+}
