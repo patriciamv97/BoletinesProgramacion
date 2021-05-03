3efaae0f94bc7f0031eefdadbcfd4809c882eb96
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-05-03 08:54:31 +0200

    Acomodo la clase main

--- src/com/programacion3/patri/boletin25/Factura.java
+++ src/com/programacion3/patri/boletin25/Factura.java
@@ -11,14 +11,15 @@ public class Factura {
         ArrayList<Barco> barcos = new ArrayList<Barco>();
         Barco yate1 = new Yates("aabb", 10, 3, 220, 3);
         Barco velero1 = new Veleros("bbaa", 20, 3, 5);
+        Barco deportivo = new EmbacacionesDeportivasAMotor("ccaa",10,2,220);
         barcos.add(yate1);
         barcos.add(velero1);
+        barcos.add(deportivo);
+
         for (Barco ele : barcos) {
             precio=(ele.getNumDias()* ele.modulo());
-            System.out.println("MATRICULA : "+ele.getMatricula());
-            System.out.println("ESLORA : "+ele.getEslora());
-            System.out.println("PRECIO : "+precio);
-
+            System.out.println("***FACTURA***"+"\n"+"MATRICULA : "+ele.getMatricula() +"\n"+"ESLORA : "
+                    +ele.getEslora() +"\n" +"PRECIO : "+precio+"\n");
         }
     }
 }
